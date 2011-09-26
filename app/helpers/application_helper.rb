7fb27fe443e9f8a9a3d7068dc36a7b6081f27c87
hlmtre <hellmitre@gmail.com>
2011-09-26 13:34:33 -0700

    routing for stylesheets working

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  # assign page title on per-page basis; if unnasigned assign generic
+  def title
+    base_title = "Ruby on Rails Sample App"
+    if @title.nil?
+      base_title
+    else
+      "#{base_title}" | "#{title}"
+    end
+  end
 end
